f91b272ccb6b0ccc67ec6008af2d5ec276d14ef2
Jia Lu <jialu@Jias-MacBook-Pro.local>
2016-01-14 16:26:19 -0800

    Delete Included and annotation functions

--- bin/Stitch-seq_Aligner.py
+++ bin/Stitch-seq_Aligner.py
@@ -3,7 +3,7 @@
 # Name:        Stitch-seq_Aligner
 # Purpose:
 #
-# Author:      Pengfei
+# Author:      Pengfei, Xiaoyi
 #
 # Created:     21/08/2012
 # Modified:    03/09/2013
@@ -42,7 +42,6 @@ def ParseArg():
     groupBowtie.add_argument('-bt2', '--bowtie2_path', type=str, metavar="bowtie2_path", help="path for bowtie2 executive")
     p.add_argument('-bl', '--blat_path', type=str, metavar='blat_path', help="path for the blat executive (for miRNA only)", default='blat')
     p.add_argument('-f','--fastq_to_fasta_path', dest='f2fpath', default='fastq_to_fasta', type=str, metavar='fastq_to_fasta_path', help="path for the fastq_to_fasta executive (for miRNA only)")
-    p.add_argument('-u','--unique',action="store_true",help="set to only allow unique alignment")
     p.add_argument('-s','--samtools_path',dest='spath', type=str,metavar='samtool_path',help="path for samtools executive",default='samtools')
     p.add_argument('--ref',type=str,metavar='rr',default="mm9",help="Reference seq files, references will be used in the order specified",nargs="*")
 
@@ -60,11 +59,6 @@ def ParseArg():
     # Transcript biotype
 
     p.add_argument('--reftype',type=str,metavar='rt',default="genome",help="Reference types: (miRNA, genome, transcript, other)",nargs="*")
-    #p.add_argument('--ref2',type=str,metavar='rr',default="none",help="Reference seq for both parts (or the first part if --ref2 is specified), references will be used in the order specified",nargs="*")
-    #p.add_argument('--ref2type',type=str,metavar='rt',default="none",help="Reference types: (miRNA, genome, transcript, other)",nargs="*")
-    #p.add_argument('-a','--annotation',type=str,help='If specified, include the RNA type annotation for each aligned pair, need to give bed annotation RNA file')
-    #p.add_argument("-A","--annotationGenebed",dest="db_detail",type=str,help="annotation bed12 file for lincRNA and mRNA with intron and exon")
-    #p.add_argument("-R","--annotationRepeat",dest="db_repeat",type=str,help="annotation bed6 file from repeatMasker")
     p.add_argument("-P", '--part_num', dest='partNum', type=check_negative, default=1, help="Specify which part (1 or 2) of the paired read is being mapped (this is used for strand matching purposes).")
     p.add_argument("-nostr", "--ignore_strand", dest="nostr", action = "store_true", help="Reads mapped onto the wrong strand will be considered as not mapped by default. Set this flag to ignore strand information.")
     p.add_argument('-p','--threads', type=check_negative, dest='nthreads', default=1, help="Number of threads used in bowtie mapping.")
@@ -280,175 +274,7 @@ def blat_annotation(outputfilename, typename, readfilename, unmapfilename, anno 
 
     return newanno
 
-#class ensemblSeq:
-#
-#    def getSubType(self, start, end):
-#        # notice that this is relative to the RNA sequence
-#        # therefore, just compare directly with self.utrlength5 or self.utrlength3 should be enough
-#       
-#        # return format: [type, name, subtype]
-#        # first find the midpoint of the region
-#        midpoint = (start + end) / 2
-#        if midpoint < self.utrlength5:
-#            return "utr5"
-#        elif midpoint >= self.length - self.utrlength3:
-#            return "utr3"
-#        else:
-#            if self.biotype == "lincRNA" or self.biotype == "protein_coding":
-#                return "exon"
-#            elif "intron" in self.biotype:
-#                return "intron"
-#            else:
-#                return "."
-#    
-#    def getAnnotation(self, start, end):
-#        return [self.biotype, self.genename, self.getSubType(start, end)]
-#
-#    #def getGeneID(self):
-#    #    return self.geneID
-#
-#    def __init__(self, name, seq):
-#        # notice that the ">" in name is chopped
-#        self.longname = name.strip()
-#        tokens = name.split("|")
-#        self.transID = tokens[0]
-#        self.strand = (int(tokens[3]) > 0)
-#        self.tss = int(tokens[1]) if self.strand else int(tokens[2])
-#        self.tes = int(tokens[2]) if self.strand else int(tokens[1])
-#        self.genename = tokens[8]
-#        if not self.genename:
-#            self.genename = self.transID
-#        #self.geneID = tokens[9]
-#        #if not self.geneID:
-#        #    self.geneID = self.transID
-#        self.biotype = tokens[9]
-#
-#        utrstarts5 = (tokens[4] if self.strand else tokens[5]).split(";")
-#        utrends5 = (tokens[5] if self.strand else tokens[4]).split(";")
-#        utrstarts3 = (tokens[6] if self.strand else tokens[7]).split(";")
-#        utrends3 = (tokens[7] if self.strand else tokens[6]).split(";")
-#
-#        self.utrlength5 = 0
-#        self.utrlength3 = 0
-#        if tokens[4]:
-#            for i in xrange(len(utrstarts5)):
-#                self.utrlength5 += abs(int(utrends5[i]) - int(utrstarts5[i]))
-#
-#        if tokens[6]:
-#            for i in xrange(len(utrstarts3)):
-#                self.utrlength3 += abs(int(utrends3[i]) - int(utrstarts3[i]))
-#
-#        self.seq = seq.strip()
-#        self.length = len(self.seq)
-#
-#
-#def otherlib_annotation(outputbam, anno, readfilename, unmapfilename, annotationfile = 'misc', requireUnique = False, posstrand = True, strandenforced = False, results_dict = dict()):
-#
-#    # outputbam is a pysam file
-#    # annotationfile can be:
-#    # "misc", "guess" from RNA name
-#    # or a known annotation file (for example, rna.fa from NCBI)
-#
-#    newdict = dict()
-#    funmap = open(unmapfilename, 'w')
-#
-#    if anno and annotationfile != 'misc':
-#        refdic = dict()
-#        if not 'fa' in annotationfile:
-#            annotationfile += ".fa"
-#
-#        # this will be a dictionary of ensembl entries
-#        fref = open(annotationfile, 'r')
-#        refname = ''
-#        refseq = ''
-#        refkey = ''
-#
-#        for line in fref:
-#            if line.startswith(">"):
-#                if refkey:
-#                    # there is an old ref there
-#                    if refkey in refdic:
-#                        print >> sys.stderr, refkey
-#                    refdic[refkey] = ensemblSeq(refname, refseq)
-#                    refseq = ''
-#                    refkey = ''
-#                    refname = ''
-#                refkey = line.strip(">").split("|")[0]
-#                refname = line.strip(">").strip()
-#            else:
-#                refseq += line.strip()
-#
-#        if refkey:
-#            if refkey in refdic:
-#                print >> sys.stderr, refkey
-#            refdic[refkey] = ensemblSeq(refname, refseq)
-#        fref.close()
-#
-#    for record in outputbam:
-#        name = record.qname.split(" ")[0]
-#        try:
-#            record.opt('XS')
-#            unique = False
-#        except:
-#            unique = True
-#        if ((not requireUnique) or unique) and ((not strandenforced) or record.is_reverse != posstrand) and not record.is_unmapped:
-#            # this is a suitable entry
-#            strand = "+"
-#            if record.is_reverse:
-#                strand = "-"
-#            if record.is_reverse == posstrand:
-#                # strand is wrong
-#                strandcol = 'NonProperStrand'
-#            else:
-#                strandcol = 'ProperStrand'
-#            if anno:
-#                if annotationfile == 'misc':
-#                    rnaname = outputbam.getrname(record.tid).split(" ")[0]
-#                    rnaid = rnaname
-#                    if rnaname.startswith("NONMMUT"):
-#                        # This is a lncRNA from NONCODE
-#                        rnatype = "ncRNA"
-#                        rnasubtype = "."
-#                    else:
-#                        # tRNA
-#                        rnatype = "tRNA"
-#                        rnasubtype = "."
-#                else:
-#                    rnaensembl = refdic[outputbam.getrname(record.tid).split("|")[0]]
-#                    rnaid = rnaensembl.transID
-#                    #geneid = rnaensembl.getGeneID()
-#                    [rnatype, rnaname, rnasubtype] = rnaensembl.getAnnotation(record.aend - record.alen + 1, record.aend)
-#
-#                curr_anno_arr = (str(f) for f in [rnaid, record.aend - record.alen + 1, record.aend, strand, record.seq, annotationfile.split("/")[-1], rnatype, rnaname, rnasubtype, strandcol, record.mapq])
-#                if not name in newdict:
-#                    newdict[name] = '\t'.join(curr_anno_arr)
-#                else:
-#                    if type(newdict[name]) is str:
-#                        newdict[name] = [newdict[name]]
-#                    newdict[name].append('\t'.join(curr_anno_arr))
-#
-#            else:
-#                curr_anno_arr = (str(f) for f in [rnaid, record.aend - record.alen + 1, record.aend, strand, record.seq, annotationfile.split("/")[-1], strandcol, record.mapq])
-#                if not name in newdict:
-#                    newdict[name] = '\t'.join(curr_anno_arr)
-#                else:
-#                    if type(newdict[name]) is str:
-#                        newdict[name] = [newdict[name]]
-#                    newdict[name].append('\t'.join(curr_anno_arr))
-#
-#        elif record.is_unmapped:
-#            # write unmapped reads
-#            seq = record.seq
-#            if record.is_reverse:
-#                seq = revcomp(record.seq, rev_table)
-#            unmap_rec = SeqRecord(Seq(seq, IUPAC.unambiguous_dna), id = name, description='')
-#            SeqIO.write(unmap_rec, funmap, "fasta")
-#
-#    funmap.close()
-#    
-#    newanno = dict(results_dict.items() + newdict.items())
-#    return newanno
-#
+
 def bowtie_align(b_path,read,ref,s_path,bowtie2,numOfThreads,nOffrate,reftype,recovering,unmap,outputPath):
     # b_path: bowtie path;
     # s_path: samtools path;
@@ -509,83 +335,7 @@ def bowtie_align(b_path,read,ref,s_path,bowtie2,numOfThreads,nOffrate,reftype,re
         print >> sys.stderr, 'Old file exists, recovery in process.'
     return align
 
-def Included(record,RequireUnique):
-    # record is a pysam read
-    # non-unique alignment in Bowtie2 has 'XS' tag: https://www.biostars.org/p/18965/ 
-    if RequireUnique:
-        try:
-            record.opt('XS')
-            unique=False
-        except:
-            unique=True
-    else:
-        unique=True # not consider unique
-    return (not record.is_unmapped)&unique
 
-#def genome_annotation(outputbam, annotationfile, detail, annotationRepeat, readfilename, unmapfilename, strandenforced = False, posstrand = True, requireUnique = False, results_dict = dict()):
-#    # annotationfile is annotation file
-#    # detail is db_detail file
-#
-#    if annotationfile:
-#        dbi1=DBI.init(annotationfile,"bed")
-#        dbi2=DBI.init(detail,"bed")
-#        dbi3=DBI.init(annotationRepeat,"bed")
-#    
-#    newdict = dict()
-#    funmap = open(unmapfilename, 'w')
-#
-#    for record in outputbam:
-#        # print >> sys.stderr, record.qname
-#        IsMapped = False
-#
-#        if Included(record, requireUnique):
-#            strandactual = ("+" if posstrand else "-")
-#            strand = "+"
-#            if record.is_reverse:
-#                strandactual = ("-" if posstrand else "+")
-#                strand = "-"
-#            if annotationfile:
-#                bed=Bed([outputbam.getrname(record.tid), record.pos, record.aend,'.',0.0,strandactual])
-#                [typ, name, subtype, strandcol] = annotation(bed,dbi1,dbi2,dbi3)
-#                if (not strandenforced) or strandcol == 'ProperStrand':
-#                    curr_anno_arr = (str(f) for f in [outputbam.getrname(record.tid), record.pos, record.aend, strand, record.seq, 'genome', typ, name, subtype, strandcol, record.mapq])
-#                    if not record.qname in newdict:
-#                        newdict[record.qname] = '\t'.join(curr_anno_arr)
-#                        if not Included(record, True):
-#                            # not unique
-#                            newdict[record.qname] = [newdict[record.qname]]
-#                    else:
-#                        if type(newdict[record.qname]) is str:
-#                            newdict[record.qname] = [newdict[record.qname]]
-#                        newdict[record.qname].append('\t'.join(curr_anno_arr))
-#                    IsMapped = True
-#            else:
-#                strandcol = '.'
-#                curr_anno_arr = (str(f) for f in [outputbam.getrname(record.tid), record.aend - record.alen + 1, record.aend, strand, record.seq, 'genome', strandcol, record.mapq])
-#                if not record.qname in newdict:
-#                    newdict[record.qname] = '\t'.join(curr_anno_arr)
-#                    if not Included(record, True):
-#                        # not unique
-#                        newdict[record.qname] = [newdict[record.qname]]
-#                else:
-#                    if type(newdict[record.qname]) is str:
-#                        newdict[record.qname] = [newdict[record.qname]]
-#                    newdict[record.qname].append('\t'.join(curr_anno_arr))
-#                IsMapped = True
-#
-#        if not IsMapped:
-#            # output all pairs that cannot be mapped on both sides as unmaped pairs into two fasta file
-#            seq = record.seq
-#            if record.is_reverse:
-#                seq = revcomp(record.seq, rev_table)
-#            unmap_rec = SeqRecord(Seq(seq, IUPAC.unambiguous_dna), id = record.qname, description='')
-#            SeqIO.write(unmap_rec, funmap, "fasta")
-#    
-#    funmap.close()
-#    
-#    newanno = dict(results_dict.items() + newdict.items())
-#    return newanno
-#
 
 def createPath(path):
     try:
@@ -694,62 +444,13 @@ def Main():
 
         if reftype.lower() == "mirna":
             outputfile, readused = blat_align(args.blat_path, readfile, reffile, args.f2fpath, inRecoveryFrag, unmap_read, args.mirnalen, output)
-            annodictentry = blat_annotation(outputfile, reftype, readused, unmap_read, annotation, args.unique, strand, strandenforced, 2, annodictentry)
+            annodictentry = blat_annotation(outputfile, reftype, readused, unmap_read, annotation, False, strand, strandenforced, 2, annodictentry)
         else:
             outputbam = bowtie_align(args.bowtie_path, readfile, reffile, args.spath, args.bowtie2, nThreads, nOffrate, reftype.lower(), inRecoveryFrag, unmap_read, output)
-            #if reftype.lower() == "genome":
-            #    annodictentry = genome_annotation(outputbam, annotation, db_detail, db_repeat, readfile, unmap_read, strandenforced, strand, args.unique, annodictentry)
-            #else:
-            #    if reftype.lower() == 'other':
-            #        annofile = 'misc'
-            #    else:
-            #        annofile = reffile
-            #    annodictentry = otherlib_annotation(outputbam, annotation, readfile, unmap_read, annofile, args.unique, strand, strandenforced, annodictentry)
 
         readfile = unmap_read
         gc.collect()
 
-    #for entry, value in annodictlist[0].iteritems():
-    #    if entry in annodictlist[1]:
-            # this is a pair
-    #        if type(value) is str:
-    #            if type(annodictlist[1][entry]) is str:
-    #                print '\t'.join(str(f) for f in [value, entry, annodictlist[1][entry],'OneToOne'])
-    #            else:
-    #                for item2 in annodictlist[1][entry]:
-    #                    print '\t'.join(str(f) for f in [value, entry, item2, 'OneToMany'])
-    #        else:
-    #            for item1 in value:
-    #                if type(annodictlist[1][entry]) is str:
-    #                    print '\t'.join(str(f) for f in [item1, entry, annodictlist[1][entry], 'ManyToOne'])
-    #                else:
-    #                    for item2 in annodictlist[1][entry]:
-    #                        print '\t'.join(str(f) for f in [item1, entry, item2, 'ManyToMany'])
-
-    #for iFrag in xrange(2):
-#        tmp = readfilelist[i].split(".")
-#        fragoutfile = ".".join(tmp[:-1]) + ".pairOutput"
-#        fReadOut = open(fragoutfile, 'w')
-
-        # annodictlistentry = annodictlist[iFrag]
-#        print >> sys.stderr, "====== Mapping result, Pair ", i + 1, " ======"
-#        print >> sys.stderr, "Number of Unique LinkerIDs: ", len(annodictentry)
-#        numOfOnes = 0
-#        totalLines = 0
-#        for entry, value in annodictentry.iteritems():
-#            if type(value) is str:
-#                numOfOnes += 1
-#                totalLines += 1
-#                fReadOut.write('\t'.join(str(f) for f in [value, entry, 'One']) + os.linesep)
-#            else:
-#                totalLines += len(value)
-#                for item in value:
-#                    fReadOut.write('\t'.join(str(f) for f in [item, entry, 'Many']) + os.linesep)
-#
-#        print >> sys.stderr, "Number of 'One's: ", numOfOnes
-#        print >> sys.stderr, "Number of lines: ", totalLines
-#
-#        fReadOut.close()
                     
 if __name__ == '__main__':
     Main()
